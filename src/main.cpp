64a6d8b64932ded393bb36b5abc1c3e587f2a7dc
Alex Rowley <alex.rowley@gmail.com>
2025-02-16 18:35:08 +0000

    enable more debugging

--- src/main.cpp
+++ src/main.cpp
@@ -64,3 +64,16 @@ int main(){
     return 0;
 }
 
+void vApplicationTickHook (void)
+{
+};
+
+void vApplicationStackOverflowHook(TaskHandle_t Task, char *pcTaskName)
+{
+    panic("stack overflow (not the helpful kind) for %s\n", *pcTaskName);
+}
+
+void vApplicationMallocFailedHook(void)
+{
+    panic("Malloc Failed\n");
+};
